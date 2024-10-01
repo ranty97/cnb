dd66218bc5aebe293d11ec363d274ef1b4758e12
ranty97 <googacc9091@gmail.com>
2024-10-02 01:04:02 +0300

    redefenition com funcs

--- internal/com/com.go
+++ internal/com/com.go
@@ -8,7 +8,7 @@ import (
 
 type Port struct {
 	Name   string
-	Speed  uint
+	Speed  int
 	Parity serial.Parity
 }
 
@@ -83,4 +83,46 @@ func ReceiveData(portName string, mode *serial.Mode) (string, error) {
 		return "", err
 	}
 	return string(buf), nil
+}
+
+func (p Port) SendBytes(data []byte) {
+	port, err := serial.Open(p.Name, &serial.Mode{BaudRate: p.Speed, Parity: p.Parity})
+	if err != nil {
+		log.Println("cannot open port")
+	}
+	n, err := port.Write(data)
+	log.Println("Written", n, "bytes")
+	err = port.Close()
+	if err != nil {
+		return
+	}
+}
+
+func (p Port) SendPacket(packet Packet) {
+	p.SendBytes(packet.SerializePacket())
+}
+
+func (p Port) ReceiveBytes() ([]byte, error) {
+	port, err := serial.Open(p.Name, &serial.Mode{BaudRate: p.Speed, Parity: p.Parity})
+	if err != nil {
+		log.Fatal("cannot open port")
+	}
+
+	buff := make([]byte, 256)
+	var n, readErr = port.Read(buff)
+	if readErr != nil {
+		return nil, err
+	}
+	err = port.Close()
+
+	log.Printf("Recieved %d bytes\n", n)
+	return buff[:n], nil
+}
+
+func (p Port) ReceivePacket() (Packet, error) {
+	data, err := p.ReceiveBytes()
+	if err != nil {
+		return Packet{}, err
+	}
+	return DeserializePacket(data)
 }
